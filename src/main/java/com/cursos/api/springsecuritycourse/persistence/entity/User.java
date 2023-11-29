5f5d117b487ae77bbe971a5c6e35333edb6889b3
aczambrano3 <zambranoanderson84@yahoo.es>
2023-11-28 19:46:41 -0500

    aplicando roles y seguridades al proyecto

---
+++ src/main/java/com/cursos/api/springsecuritycourse/persistence/entity/User.java
@@ -0,0 +1,67 @@
+package com.cursos.api.springsecuritycourse.persistence.entity;
+
+import com.cursos.api.springsecuritycourse.persistence.util.Role;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(role == null) return null;
+        if(role.getPermissions() == null) return null;
+
+        return role.getPermissions().stream().map(e->{
+            String permission = e.name();
+            return new SimpleGrantedAuthority(permission);
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
